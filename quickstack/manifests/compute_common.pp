86e7ef6f84555d6bcaa1c4b0ecaa2c153cd0fd1d
root <root@foreman.moc.ne.edu>
2016-03-16 22:40:12 +0000

    added instructions retired plugin to be installed

--- quickstack/manifests/compute_common.pp
+++ quickstack/manifests/compute_common.pp
@@ -388,7 +388,8 @@ class quickstack::compute_common (
        "puppet:///modules/sensu/plugins/keystone-token-metrics.rb",
        "puppet:///modules/sensu/plugins/nova-hypervisor-metrics.py",
        "puppet:///modules/sensu/plugins/nova-server-state-metrics.py",
-       "puppet:///modules/sensu/plugins/cpu-pcnt-usage-metrics.rb"
+       "puppet:///modules/sensu/plugins/cpu-pcnt-usage-metrics.rb",
+       "puppet:///modules/sensu/plugins/instructions-retired.rb"
     ]
   }
 
