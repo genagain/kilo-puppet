dffa8f98f1d8fc3d183644b08211a82712e15c93
root <root@foreman.moc.ne.edu>
2016-03-21 17:52:44 +0000

    added reference to disk metrics plugin

--- quickstack/manifests/compute_common.pp
+++ quickstack/manifests/compute_common.pp
@@ -391,6 +391,7 @@ class quickstack::compute_common (
        "puppet:///modules/sensu/plugins/nova-hypervisor-metrics.py",
        "puppet:///modules/sensu/plugins/nova-server-state-metrics.py",
        "puppet:///modules/sensu/plugins/cpu-pcnt-usage-metrics.rb",
+       "puppet:///modules/sensu/plugins/disk-metrics.rb",
        "puppet:///modules/sensu/plugins/instructions-retired.rb"
     ]
   }
