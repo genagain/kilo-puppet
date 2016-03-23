3d901e9b823649ea9148d3421e60f091891f05ab
genagain <ohta.g@husky.neu.edu>
2016-03-23 15:58:02 +0000

    removed all the instructions retired stuff

--- quickstack/manifests/compute_common.pp
+++ quickstack/manifests/compute_common.pp
@@ -401,8 +401,7 @@ class quickstack::compute_common (
        "puppet:///modules/sensu/plugins/nova-hypervisor-metrics.py",
        "puppet:///modules/sensu/plugins/nova-server-state-metrics.py",
        "puppet:///modules/sensu/plugins/cpu-pcnt-usage-metrics.rb",
-       "puppet:///modules/sensu/plugins/disk-metrics.rb",
-       "puppet:///modules/sensu/plugins/instructions-retired.rb"
+       "puppet:///modules/sensu/plugins/disk-metrics.rb"
     ]
   }
 
