2545cc07e5cd13cb6d174e759e0827797491ab66
genagain <ohta.g@husky.neu.edu>
2016-04-04 18:42:19 +0000

    commenting out filebeat temporary

--- quickstack/manifests/controller_common.pp
+++ quickstack/manifests/controller_common.pp
@@ -837,17 +837,17 @@ class quickstack::controller_common (
     cron_min       => $backups_min,
   }
   
-  class { '::filebeat':
-    outputs => {
-      'logstash'  => {
-      'host'        =>  $elasticsearch_host,
-      'loadbalance' => true
-      }
-    },
-    prospectors => {
-      "paths" => ["/var/log/*.log", "/var/log/keystone/*", "/var/log/glance/*","/var/log/ceph/*", "/var/log/ceilometer/*", "/var/log/cinder/*", "/var/log/horizon/*", "/var/log/neutron/*", "/var/log/nova/*", "/var/log/sensu/*", "/var/log/puppet/*", "/var/log/rabbitmq/*"],
-      "input_type" => "log"
-    }
-  }
+#  class { '::filebeat':
+#    outputs => {
+#      'logstash'  => {
+#      'host'        =>  $elasticsearch_host,
+#      'loadbalance' => true
+#      }
+#    },
+#    prospectors => {
+#      "paths" => ["/var/log/*.log", "/var/log/keystone/*", "/var/log/glance/*","/var/log/ceph/*", "/var/log/ceilometer/*", "/var/log/cinder/*", "/var/log/horizon/*", "/var/log/neutron/*", "/var/log/nova/*", "/var/log/sensu/*", "/var/log/puppet/*", "/var/log/rabbitmq/*"],
+#      "input_type" => "log"
+#    }
+#  }
 
 }
